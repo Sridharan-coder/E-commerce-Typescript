f0d15647c411d69ba9edd5e00558e2a36a0b99ec
Sridharan-coder <sridharan.r@mitrahsoft.com>
2025-01-02 18:00:57 +0530

    Application completed

--- backend/public/Controllers/smsSender.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const accountSid = process.env.Account_Sid;
-const authToken = process.env.Auth_Token;
-const client = require('twilio')(accountSid, authToken);
-client.verify.v2.services(process.env.SMS_Sender_Verification_Service)
-    .verifications
-    .create({ to: process.env.SMS_PhoneNumber, channel: 'sms' })
-    .then((verification) => console.log(verification.sid));
